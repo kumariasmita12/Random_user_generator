814cf2de00958146a851fa69b42b348517399823
kumariasmita12 <kumariasmita12>
2018-08-16 10:16:17 +0530

    master

---
+++ main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+	  $("#name").text((data.results[0].name.first)+ " " + data.results[0].name.last)
+	  $(".img-circle").attr('src',data.results[0].picture.large)
+	  	  $("#email").text((data.results[0].email))
+		   $("#gender").text((data.results[0].gender))
+		    	  $("#phone").text((data.results[0].phone))
+    console.log(data.results[0].name.first);
+  }
+});
+	 
+});
